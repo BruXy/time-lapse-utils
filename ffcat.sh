af81caeb5dde9e4968a28645d2d3b0ebde30c642
Martin Bruchanov <bruchy@gmail.com>
2018-10-19 20:08:52 -0300

    Fixed issue with "Unsafe file name"

--- ffcat.sh
+++ ffcat.sh
@@ -3,38 +3,38 @@
 #
 #
 TMP_FILE=/tmp/.ffcat-$$.txt
-OVERWRITE='' 
+OVERWRITE=''
 
 function print_help() {
 cat << EOF
 
 Usage:
-	$0 [-y] [file1 file2 file3... ] output
+    $0 [-y] [file1 file2 file3... ] output
 
-	-y ... Overwrite output file without asking
+    -y ... Overwrite output file without asking
 EOF
 }
 
 function ifexist() {
-	if [ -f "$1" ] 
-	then
-		printf "$0: overwrite ‘$1’? " >&2
-		read input
-		[[ $input != 'y' ]] && exit 0
-	fi
+    if [ -f "$1" ] 
+    then
+        printf "$0: overwrite ‘$1’? " >&2
+        read input
+        [[ $input != 'y' ]] && exit 0
+    fi
 }
 
 if [ $# -le 1 ] 
 then
-	print_help
-	exit 1
+    print_help
+    exit 1
 fi
 
 # Check if overwrite is invoked
-if [[ $1 == '-y' ]] 
+if [[ $1 == '-y' ]]
 then
-	OVERWRITE='-y'
-	shift
+    OVERWRITE='-y'
+    shift
 fi
 
 file_list=( $@ )
@@ -42,18 +42,20 @@ last=${file_list[-1]}
 [ -z $OVERWRITE ] && ifexist "$last"
 
 # Create a file list, do not include the last file (result)
-for i in ${file_list[*]} 
+for i in ${file_list[*]}
 do
-	filename="$PWD/$i"
-	if [ -f $filename ] && [[ $i != $last ]]  ; then
-		echo "file '$filename'"
-	else
-		[ $i != $last ] && echo "$0: file '$filename' not found!" >&2
-	fi
-done > $TMP_FILE 
+    filename="$PWD/$i"
+    if [ -f $filename ] && [[ $i != $last ]]  ; then
+        echo "file '$filename'"
+    else
+        [ $i != $last ] && echo "$0: file '$filename' not found!" >&2
+    fi
+done > $TMP_FILE
 
 # Concatenate video files
-ffmpeg $OVERWRITE -f concat -i $TMP_FILE -codec copy $last
+# ffmpeg was complaining about "Unsafe file name", using -safe 0
+# helped to fix the problem.
+ffmpeg $OVERWRITE -safe 0 -f concat -i $TMP_FILE -codec copy $last
 
 rm -f $TMP_FILE
 
